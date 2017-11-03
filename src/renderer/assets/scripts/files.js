f94c2446497c3153e9f994fed9b290a408477eea
Khoerodin <khoerodin@live.com>
2017-11-04 06:10:07 +0700

    Revert "Change button name"
    This reverts commit 4b2a8c05fed93a5f22820e565a0e311403512fdd.

--- src/renderer/assets/scripts/files.js
+++ src/renderer/assets/scripts/files.js
@@ -9,50 +9,35 @@ export const fileAction = function () {
     var btnSave = document.querySelector('#save')
     var content = document.querySelector('.ql-editor')
 
-    btnLoad.addEventListener('click', () => {
-      dialog.showOpenDialog(
-        {
-          filters: [{
-            name: 'Markdown',
-            extensions: ['md']
-          }]
-        },
-        (filePaths) => {
-          if (filePaths === undefined) {
-            return
-          }
-
-          var filePath = filePaths[0]
-
-          try {
-            content.innerHTML = fs.readFileSync(filePath, 'utf-8')
-
-            console.log('Loaded file:' + filePath)
-          } catch (err) {
-            console.log('Error reading the file: ' + JSON.stringify(err))
-          }
+    btnLoad.addEventListener('click', function () {
+      dialog.showOpenDialog(function (filePaths) {
+        if (filePaths === undefined) {
+          return
         }
-      )
+
+        var filePath = filePaths[0]
+
+        try {
+          content.innerHTML = fs.readFileSync(filePath, 'utf-8')
+
+          console.log('Loaded file:' + filePath)
+        } catch (err) {
+          console.log('Error reading the file: ' + JSON.stringify(err))
+        }
+      })
     })
 
     btnSave.addEventListener('click', () => {
-      dialog.showSaveDialog(
-        {
-          filters: [{
-            name: 'Markdown',
-            extensions: ['md']
-          }]
-        },
-        (filePath) => {
-          if (filePath === undefined) {
-            return
-          }
-          fs.writeFile(filePath, content.innerHTML, (err) => {
-            if (err) throw err
-            console.log('The file has been saved!')
-          })
+      dialog.showSaveDialog((filePath) => {
+        if (filePath === undefined) {
+          return
         }
-      )
+
+        fs.writeFile(filePath, content.innerHTML, (err) => {
+          if (err) throw err
+          console.log('The file has been saved!')
+        })
+      })
     })
   })
 }
