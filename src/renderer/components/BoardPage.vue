80a1dd57f7dd65e761e9dc3c8b3bd6d273c42955
Khoerodin <khoerodin@live.com>
2017-11-04 18:35:59 +0700

    make more simply scripts

--- src/renderer/components/BoardPage.vue
+++ src/renderer/components/BoardPage.vue
@@ -9,8 +9,8 @@
           </div>
         </div>
       </div>
+      <div id="editor"></div>
     </div>
-    <div id="editor" class="markdown-body"></div>
   </section>
 </template>
 
@@ -40,43 +40,8 @@
 </script>
 
 <style lang="scss">
-  @import '../../../node_modules/quill/dist/quill.bubble.css';
-  @import '../../../node_modules/github-markdown-css/github-markdown.css';
+  @import '~quill/dist/quill.bubble.css';
+  @import '~bulma';
   @import '../assets/styles/fonts.css';
   @import '../assets/styles/style.css';
-  @import '../assets/styles/padding-margin.css';
-  @import '~bulma/sass/utilities/initial-variables';
-  $navbar-height: 1.7rem;
-  $section-padding: 1rem 1.5rem;
-  @import '~bulma';
-
-  html {
-    overflow-y: hidden !important;
-  }
-
-  .bg {
-    background-color: #555;
-  }
-
-  .markdown-body {
-    box-sizing: border-box;
-    min-width: 200px;
-    max-width: 980px;
-    margin: 0 auto;
-    padding: 45px;
-    font-family: "Source Sans Pro", sans-serif;
-    font-size:18px;
-    line-height:135%;
-    color: rgba(0,0,0,0.8);
-  }
-
-  .markdown-body p {
-    font-family:'Noto Serif',serif;
-  }
-
-  @media (max-width: 767px) {
-    .markdown-body {
-      padding: 45px 15px 15px 15px;
-    }
-  }
 </style>
