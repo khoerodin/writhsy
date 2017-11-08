bf4e4d97cb2b975faeac2078a3176878c47bae66
Khoerodin <khoerodin@live.com>
2017-11-08 11:27:45 +0700

    save opened file

--- src/renderer/assets/scripts/components/BoardPage/modules/SaveAction.js
+++ src/renderer/assets/scripts/components/BoardPage/modules/SaveAction.js
@@ -1,8 +1,7 @@
 'use strict'
 
 export const SaveAction = function () {
-  const fileState = window.localStorage.getItem('fileState')
-  console.log(JSON.parse(fileState))
+  const fileState = JSON.parse(window.localStorage.getItem('fileState'))
 
   const fs = require('fs')
   const {dialog} = require('electron').remote
@@ -10,21 +9,34 @@ export const SaveAction = function () {
   const content = document.querySelector('#editor')
   const markdownContent = toMarkdown(content.innerHTML)
 
-  dialog.showSaveDialog(
-    {
-      filters: [{
-        name: 'Markdown',
-        extensions: ['md']
-      }]
-    },
-    (filePath) => {
-      if (filePath === undefined) {
-        return
-      }
-      fs.writeFile(filePath, markdownContent, (err) => {
-        if (err) throw err
-        document.querySelector('title').innerText = filePath
-      })
+  console.log(fileState)
+
+  if (fileState.state === 'open') {
+    if (fileState.filePath === undefined) {
+      return
     }
-  )
+
+    fs.writeFile(fileState.filePath, markdownContent, (err) => {
+      if (err) throw err
+      document.querySelector('title').innerText = fileState.filePath + ' - saved again'
+    })
+  } else {
+    dialog.showSaveDialog(
+      {
+        filters: [{
+          name: 'Markdown',
+          extensions: ['md']
+        }]
+      },
+      (filePath) => {
+        if (filePath === undefined) {
+          return
+        }
+        fs.writeFile(filePath, markdownContent, (err) => {
+          if (err) throw err
+          document.querySelector('title').innerText = filePath
+        })
+      }
+    )
+  }
 }
