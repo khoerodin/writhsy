a1dc2377feaac457b0294d593b3a1474edc91673
Khoerodin <khoerodin@live.com>
2017-11-08 11:06:10 +0700

    remove landing page

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,11 +11,6 @@ export default new Router({
       component: require('@/components/BoardPage').default
     },
     {
-      path: '/landing',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
       path: '*',
       redirect: '/'
     }
