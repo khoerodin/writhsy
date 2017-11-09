77e011ec5a738cd99e3d45a5039fe43f9ce7bc9e
Khoerodin <khoerodin@live.com>
2017-11-09 10:19:18 +0700

    remove unwanted span

--- src/renderer/assets/scripts/components/BoardPage/modules/Editor.js
+++ src/renderer/assets/scripts/components/BoardPage/modules/Editor.js
@@ -3,7 +3,15 @@
 import {MediumEditor} from 'medium-editor'
 
 const Editor = function () {
-  return new MediumEditor('#editor')
+	const element = document.querySelector('#editor')
+	element.addEventListener("input", function () {
+		var spans = element.getElementsByTagName('span')
+		for (var i = 0; i < spans.length; i++) {
+			spans[i].outerHTML = spans[i].innerHTML
+		}
+	})
+
+  return new MediumEditor(element)
 }
 
 export default {
