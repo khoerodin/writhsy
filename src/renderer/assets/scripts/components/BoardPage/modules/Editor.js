fe1366671d988dcfbc43c2d88da35842dd18ce1a
Khoerodin <khoerodin@live.com>
2017-11-09 11:05:56 +0700

    fix jslint error

--- src/renderer/assets/scripts/components/BoardPage/modules/Editor.js
+++ src/renderer/assets/scripts/components/BoardPage/modules/Editor.js
@@ -3,13 +3,13 @@
 import {MediumEditor} from 'medium-editor'
 
 const Editor = function () {
-	const element = document.querySelector('#editor')
-	element.addEventListener("input", function () {
-		var spans = element.getElementsByTagName('span')
-		for (var i = 0; i < spans.length; i++) {
-			spans[i].outerHTML = spans[i].innerHTML
-		}
-	})
+  const element = document.querySelector('#editor')
+  element.addEventListener('input', function () {
+    var spans = element.getElementsByTagName('span')
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].outerHTML = spans[i].innerHTML
+    }
+  })
 
   return new MediumEditor(element)
 }
